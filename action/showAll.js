92a3150a59d82b33d6ffdc06f14c0cc6abaac885
Fernandez Ludovic <ludovic.fernandez@zenika.com>
2015-03-06 10:30:17 +0100

    feat: gestion des clients qui ne sont pas en favoris

--- action/showAll.js
+++ action/showAll.js
@@ -1,10 +1,11 @@
 (function () {
     'use strict';
 
-    var elts = document.querySelectorAll('.tabPair input,.tabImpair input');
+    var elts = document.querySelectorAll('.scrollPane input[type="text"]');
 
     Array.prototype.forEach.call(elts, function (input) {
         input.readOnly = false;
+        input.disabled = false;
         input.removeAttribute('style');
     });
 
