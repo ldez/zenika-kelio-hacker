062ff6a0e7c2c8d7e0f8b61eefe44f0d0979c09e
Fernandez Ludovic <ludovic.fernandez@zenika.com>
2015-03-07 21:43:57 +0100

    refactor(prevWeek): remove code commented

--- action/prevWeek.js
+++ action/prevWeek.js
@@ -8,7 +8,7 @@
 
     var match = /javascript:(fcSelectionnerDate\('[\d]{8}'\))/ig.exec(oldHref);
 
-    // Create code to execute
+        // Create code to execute
     var code = match[1];
 
     // Execute code
@@ -18,20 +18,3 @@
     script.parentNode.removeChild(script);
 
 })();
-
-
-/*
- * How extract previous week link data (feature ?)
- */
-//var oldHref = document.querySelector('.tableContainer .filtre a').href;
-//
-//var match = /javascript:fcSelectionnerDate\('([\d]{4})([\d]{2})([\d]{2})'\)/ig.exec(oldHref);
-//
-//var currentStartWeek = new Date(match[1], match[2], match[3]);
-//
-//console.log(currentStartWeek);
-//
-//var nextStartWeek = new Date(currentStartWeek);
-//nextStartWeek.setDate(currentStartWeek.getDate() + 7)
-//
-//console.log(nextStartWeek.toISOString().split('T')[0].replace(/-/gi, ''));
